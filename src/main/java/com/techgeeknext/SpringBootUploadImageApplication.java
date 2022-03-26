3df0acbeae3168e140458ae30602be535c73a474
Amadou Diadie Coulibaly <djimdemb45@gmail.com>
2022-03-26 15:17:55 +0000

    Initial commit

---
+++ src/main/java/com/techgeeknext/SpringBootUploadImageApplication.java
@@ -0,0 +1,27 @@
+package com.techgeeknext;
+
+import javax.annotation.Resource;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.techgeeknext.service.FilesStorageService;
+
+@SpringBootApplication
+public class SpringBootUploadImageApplication implements CommandLineRunner {
+
+	@Resource
+	 FilesStorageService storageService;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUploadImageApplication.class, args);
+	}
+	
+	@Override
+	  public void run(String... arg) throws Exception {
+	    storageService.deleteAll();
+	    storageService.init();
+	  }
+	
+}
